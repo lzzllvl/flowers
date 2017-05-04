8547d0817eca0c3c8abf2862985bdfb574e64737
Greg Lee <gml2136@gmail.com>
2017-05-03 21:08:28 -0700

    still working on generating maze

--- Flowers/Flowers.cpp
+++ Flowers/Flowers.cpp
@@ -4,6 +4,8 @@
 #include "stdafx.h"
 #include "mazenode.h"
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
@@ -12,16 +14,40 @@ MazeNode* generateMaze(int numNodes) {
 	//pointer to start of maze data structure
 	MazeNode* start = new MazeNode;
 	//pointer to the most recent node
-	MazeNode* next = nullptr;
+	MazeNode* next = start;
 
 	while (--numNodes) {
-		MazeNode* nextLeft = new MazeNode;
-		MazeNode* nextRight = new MazeNode;
-		start->setLeft(nextLeft);
-		start->setRight(nextRight);
+		//Random number corresponding to sides of the Maze node
+		srand(time(NULL));
+		int which = rand() % 4 + 1;
 
-		next = nextLeft;
-		(start->getRight())->setCheese(true);
+		//create next node
+		MazeNode* connection = new MazeNode;
+
+		switch (which) {
+			//set both forward and backward pointers for both connected nodes
+			case(1):
+				next->setLeft(connection);
+				connection->setRight(next);
+				break;
+			case(2):
+				next->setRight(connection);
+				connection->setLeft(next);
+				break;
+			case(3):
+				next->setUp(connection);
+				connection->setDown(next);
+				break;
+			case(4):
+				next->setDown(connection);
+				connection->setUp(next);
+				break;
+		}
+		//set next to the next node
+		//TODO - check needed for existence of direction pointer existence
+		next = connection;
+
+		
 	}
 
 	return start; 
